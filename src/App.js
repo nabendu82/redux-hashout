84a2a1cf8993af9c3140c4603471352e3969d4e8
nabendu82 <nabendu.biswas@gmail.com>
2025-04-11 10:50:01 +0530

    Redux done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import EggContainer from './components/EggContainer';
 import store from './redux/store';
 import HookEggContainer from './components/HookEggContainer';
 import HookChickenContainer from './components/HookChickenContainer';
+import UserContainer from './components/UserContainer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <EggContainer />
         <HookEggContainer />
         <HookChickenContainer />
+        <UserContainer />
       </Provider>
     </div>
   );
